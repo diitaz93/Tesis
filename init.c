074eb1e3cf9d310dca5f26b6a1f989e26d2a258c
diitaz93 <juantrix93@hotmail.com>
2017-07-10 00:18:59 -0500

    arreglar arcotangente

--- init.c
+++ init.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+/************************* FUNCTION PROTOTYPES ************************************/
 void initial(double x[], double y[], double angle[], int N, double L);
 void screenshot(double x[], double y[], double angle[], int N);
 void new_angle(double x[], double y[], double angle[], int N, double r);
 void new_coord(double x[], double y[], double angle[], int N, double delta_t, double v, double L);
 
+/******************************** MAIN ********************************************/
 int main(int argc, char **argv)
-/* User must type number of particles N*/
+/* User must type number of particles N */
 {
   // Variables
   int N = atoi(argv[1]);// Number of particles
@@ -17,27 +19,24 @@ int main(int argc, char **argv)
   double y[N]; // y coordinate
   double angle[N]; // Angle
   double L = 1.0;// Side of square
-  double v = 0.75; // Norm of velocity
+  double v = 0.03; // Norm of velocity
   double r = 1.0; // First neighbourg radius
   double delta_t = 1.0; // Timestep
 
   // Initialization
   initial(x, y, angle, N, L);
   screenshot(x, y, angle, N);
-  printf("\n");
 
   // Temporal evolution
-  new_angle(x, y, angle, N, r);
   new_coord(x, y, angle, N, delta_t, v, L);
+  new_angle(x, y, angle, N, r);
   screenshot(x, y, angle, N);
 
-  //Return
+  // Return
   return 0;
 }
 
-/*********************************************************
-********************* FUNCTIONS **************************
-*********************************************************/
+/******************** FUNCTIONS *****************************************/
 
 // INITIALIZES ARRAYS RANDOMLY
 void initial(double x[], double y[], double angle[], int N, double L)
@@ -79,12 +78,20 @@ void screenshot(double x[], double y[], double angle[], int N)
 //UPDATES THE ANGLE OF VELOCITY
 void new_angle(double x[], double y[], double angle[], int N, double r)
 {
-  int i, j;//counters for
-  int cont; // Counter
+  int i, j;// indexes
   double sum_sin, sum_cos;
+  double at; // Temporal variable
+  
+  // Makes copy of array
+  double copy[N];
+  for (i=0;i<=N-1;i++)
+    {
+      copy[i] = angle[i];
+    }
+  
+  // Update	 
   for(i=0;i<=N-1;i++)
     {
-      cont = 0;
       sum_sin = 0;//sum of sines of neighbourgs
       sum_cos = 0;//sum of cosines of neighbourgs
       
@@ -94,52 +101,32 @@ void new_angle(double x[], double y[], double angle[], int N, double r)
 	  if ( sqrt( pow( (x[i]-x[j]), 2) + pow( (y[i]-y[j]), 2) ) < r )
 	    {
 	      // If neighbourg, sum sines and cosines of angle
-	      sum_sin+=sin(angle[j]);
-	      sum_cos+=cos(angle[j]);
-	      cont ++;
+	      sum_sin+=sin(copy[j]);
+	      sum_cos+=cos(copy[j]);
 	    }// end if
 	}// end comparison
       
-      angle[i]=atan( sum_sin/sum_cos );//new angle calculation
+      at=atan( sum_sin/sum_cos );//new angle calculation
+      // values between 0 and 2pi
+      angle[i] = at;//fabs( at - 2 * M_PI * floor( at/2*M_PI ) );
     }// end update
-}//end new_angle
+}// end new_angle
 
-//UPDATES POSITIONS
+// UPDATES POSITIONS
 void new_coord(double x[], double y[], double angle[], int N, double delta_t, double v, double L)
 {
-  int i; // Counter
+  int i; // index
   double xt, yt; // Temporal variables
   for(i=0;i<=N-1;i++)
     {
-      xt = v*cos(angle[i])*delta_t;
-      yt = v*sin(angle[i])*delta_t;
+      // New coordinate is old coordinate plus velocity times time
+      xt = x[i] + v*cos(angle[i])*delta_t;
+      yt = y[i] + v*sin(angle[i])*delta_t;
       
-      // Periodic boundary conditions in x
-      if (xt > L)
-	{
-	  x[i] = xt - 1;
-	}
-      else if (xt < 0)
-	{
-	  x[i] = xt + 1;
-	}
-      else
-	{
-	  x[i] = xt;
-	}
-      // Periodic boundary conditions in y
-      if (yt > L)
-	{
-	  y[i] = xt -1;
-	}
-      else if (yt < 0)
-	{
-	  y[i] = yt + 1;
-	}
-      else
-	{
-	  y[i] = yt;
-	}
+      // Periodic boundary conditions
+      x[i] = fabs( xt - L*floor( xt/L ) );
+      y[i] = fabs( yt - L*floor( yt/L ) );
+      
     } // end update
 } // end new_coord
 
