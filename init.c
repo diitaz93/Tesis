a4345e1c02a92c0e97785d46623704a04407bc6a
diitaz93 <juantrix93@hotmail.com>
2017-08-12 20:29:40 -0500

    L y eta como parametro

--- init.c
+++ init.c
@@ -3,26 +3,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define STEPS 10 //Number of timesteps for temporal evolution
+#define STEPS 10 //Number of timesteps (minus one due to init) for temporal evolution
 
 /************************* FUNCTION PROTOTYPES ************************************/
 void initial(double x[], double y[], double angle[], int N, double L);
 void screenshot(double x[], double y[], double angle[], int N);
-void new_angle(double x[], double y[], double angle[], int N, double r);
+void new_angle(double x[], double y[], double angle[], int N, double r, double eta);
 void new_coord(double x[], double y[], double angle[], int N, double v, double L);
 
 /******************************** MAIN ********************************************/
 int main(int argc, char **argv)
-/* User must type number of particles N */
+/* User must type number of particles N, length of square L and noise eta */
 {
   // Variables
   int N = atoi(argv[1]);// Number of particles
   double x[N]; // x coordinate
   double y[N]; // y coordinate
   double angle[N]; // Angle
-  double L = 1.0;// Side of square
-  double v = 0.03; // Norm of velocity
-  double r = 0.2; // First neighbourg radius
+  double L = atof(argv[2]);// Side of square
+  double v = 0.1; // Norm of velocity
+  double r = 0.125; // First neighbourg radius
+  double eta = atof(argv[3]); //Noise coefficient
 
   // Initialization
   initial(x, y, angle, N, L);
@@ -33,7 +34,7 @@ int main(int argc, char **argv)
   for (i=0; i<=STEPS-1; i++)
     {
       new_coord(x, y, angle, N, v, L);
-      new_angle(x, y, angle, N, r);
+      new_angle(x, y, angle, N, r, eta);
       screenshot(x, y, angle, N);
       
     }
@@ -82,7 +83,7 @@ void screenshot(double x[], double y[], double angle[], int N)
 }
 
 //UPDATES THE ANGLE OF VELOCITY
-void new_angle(double x[], double y[], double angle[], int N, double r)
+void new_angle(double x[], double y[], double angle[], int N, double r, double eta)
 {
   int i, j;// indexes
   double sum_sin, sum_cos;
@@ -107,14 +108,15 @@ void new_angle(double x[], double y[], double angle[], int N, double r)
 	  if ( sqrt( pow( (x[i]-x[j]), 2) + pow( (y[i]-y[j]), 2) ) < r )
 	    {
 	      // If neighbourg, sum sines and cosines of angle
-	      sum_sin+=sin(copy[j]);
-	      sum_cos+=cos(copy[j]);
+	      sum_sin += sin(copy[j]);
+	      sum_cos += cos(copy[j]);
 	    }// end if
 	}// end comparison
       
-      at=atan( sum_sin/sum_cos );//new angle calculation
+      at = atan( sum_sin/sum_cos );//new angle calculation
       // values between 0 and 2pi, fix arctan error (EFICIENCY ALERT!!)
       (sum_cos < 0) ? (angle[i] = at + M_PI) : (angle[i] = at);
+      angle[i] += (double)rand()*eta/(double)RAND_MAX - 0.5*eta; //Noise added
       (angle[i] < 0) ? (angle[i] += 2*M_PI) : (angle[i] = angle[i]);
     }// end update
 }// end new_angle
